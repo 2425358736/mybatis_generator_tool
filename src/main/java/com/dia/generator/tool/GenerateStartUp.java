7121f75276debf8bc3901943426bd27decd148ca
2425358736 <2425358736@qq.com>
2019-02-19 10:05:04 +0800

    自动生成

---
+++ src/main/java/com/dia/generator/tool/GenerateStartUp.java
@@ -0,0 +1,58 @@
+package com.dia.generator.tool;
+
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 开发公司：青岛海豚数据技术有限公司
+ * 版权：青岛海豚数据技术有限公司
+ * <p>
+ * GenerateStartUp
+ *
+ * @author 刘志强
+ * @created Create Time: 2019/1/17
+ */
+public class GenerateStartUp {
+
+    public static List<String> startUp(String generatorPath){
+        List<String> warnings = new ArrayList<String>();
+        try {
+            boolean overwrite = true;
+            InputStream is = new FileInputStream(Paths.get(generatorPath).toFile());
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
+        return warnings;
+    }
+}
